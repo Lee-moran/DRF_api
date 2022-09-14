23c0d67a2609e5fcc240dc98233a07c4dbb8801e
Lee-moran <leemoran06@hotmail.com>
2022-09-14 10:16:09 +0000

    added create function to likeserializer, created likeslist and likesdetail generic functions

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from  rest_framework import serializers
 from .models import Comment
 
 
@@ -22,8 +22,9 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
-    
-    class CommentDetailSerializer(CommentSerializer):
+
+
+class CommentDetailSerializer(CommentSerializer):
     """
     Serializer for the Comment model used in Detail view
     Post is a read only field so that we dont have to set it on each update
