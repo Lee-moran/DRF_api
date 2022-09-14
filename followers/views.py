ab53d19f170ced30dacf9f32defc3c5c3c1b261a
Lee-moran <leemoran06@hotmail.com>
2022-09-14 11:33:16 +0000

    folloers challenge complete with refactoriong Post and Profile

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+class FollowerList(generics.ListCreateAPIView):
+    """ List all Follower. Create a Follower if authenticated. The perform_create method associates the Follower with the logged in user. """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """ Retrieve a Follower. No Update view, as users can only Follower or unlike a post. Destroy a Follower, i.e. unFollowe a post if owner of that like """
+
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
