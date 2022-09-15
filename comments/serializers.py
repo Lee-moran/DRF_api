25b8b1856f392193c09d50b3fbb91d2a3ad86768
Lee-moran <leemoran06@hotmail.com>
2022-09-15 13:08:09 +0000

    updated

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.humanize.templatetags.humanize import naturaltime
-from  rest_framework import serializers
+from rest_framework import serializers
 from .models import Comment
 
 
@@ -8,7 +8,7 @@ class CommentSerializer(serializers.ModelSerializer):
     Serializer for the Comment model
     Adds three extra fields when returning a list of Comment instances
     """
-     owner = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
