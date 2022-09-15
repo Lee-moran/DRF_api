3417795281066ab8a0fc27804f934a144e7e6769
Lee-moran <leemoran06@hotmail.com>
2022-09-15 10:04:05 +0000

    added filter fileds to comments views and posts views

--- profiles/views.py
+++ profiles/views.py
@@ -77,7 +77,7 @@ class ProfileList(generics.ListAPIView):
         'owner__followed__created_at',
     ]
     filterset_fields = [
-        'owner__following__followed__profile'
+        'owner__following__followed__profile', 'owner__followed__owner__profile'
     ]
 
 
