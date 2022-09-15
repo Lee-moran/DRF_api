ef5634444c730f64e789a6ca8641f81e5fbb02fa
Lee-moran <leemoran06@hotmail.com>
2022-09-15 13:13:56 +0000

    changes to your settings.py file to GitHub

--- django_api/settings.py
+++ django_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import dj_database_url
 from pathlib import Path
 import os
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -36,13 +37,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    'django-api-frame.herokuapp.com',
-    'localhost',
+   os.environ.get('ALLOWED_HOST'),
+   'localhost',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
